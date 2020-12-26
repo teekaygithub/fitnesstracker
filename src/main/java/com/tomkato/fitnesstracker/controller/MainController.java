99348d75c8adb1e7ae05de56ae5e5988ca02d15f
teekaygithub <tomoyuki.kato@yahoo.com>
2020-12-26 12:44:46 -0800

    Moved the controller to its own package

--- src/main/java/com/tomkato/fitnesstracker/controller/MainController.java
+++ src/main/java/com/tomkato/fitnesstracker/controller/MainController.java
@@ -1,4 +1,4 @@
-package com.tomkato.fitnesstracker; // TODO: create separate controller package
+package com.tomkato.fitnesstracker.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
