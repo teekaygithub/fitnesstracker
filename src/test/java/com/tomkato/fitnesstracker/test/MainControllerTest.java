4b5115065356415b74ec5670d926cd76992da84d
teekaygithub <tomoyuki.kato@yahoo.com>
2020-12-30 19:25:10 -0800

    Created test to get all exercise logs at once. Changed log level to DEBUG for single-log GET test.

--- src/test/java/com/tomkato/fitnesstracker/test/MainControllerTest.java
+++ src/test/java/com/tomkato/fitnesstracker/test/MainControllerTest.java
@@ -15,6 +15,8 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.http.HttpStatus;
 
 import java.net.URL;
+import java.util.List;
+import java.util.ArrayList;
 
 import static org.mockito.Mockito.*;
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -43,13 +45,37 @@ public class MainControllerTest {
             new URL("http://localhost:" + port + "/log/1").toString(), String.class
         );
         
-        logger.info("Expected: {}", expected);
-        logger.info("Actual: {}", response.getBody());
+        logger.debug("Expected: {}", expected);
+        logger.debug("Actual: {}", response.getBody());
         assertEquals(HttpStatus.OK, response.getStatusCode());
         JSONAssert.assertEquals(expected, response.getBody(), false);
         
         verify(mockRepository, times(1)).findExerciseById(1);
     }
+	
+	@Test
+	public void getAllExercises() throws Exception {
+		Exercise log1 = new Exercise(1,"running",10,"2020-12-25");
+		Exercise log2 = new Exercise(2,"biking",20,"2020-01-01");
+		List<Exercise> logs = new ArrayList<>();
+		logs.add(log1);
+		logs.add(log2);
+		when(mockRepository.findAll()).thenReturn(logs);
+		
+		String expected =  "[{id:1,name:\"running\",duration:10,date:\"2020-12-25\"},"+
+		"{id:2,name:\"biking\",duration:20,date:\"2020-01-01\"}]";
+		
+		ResponseEntity<String> response = restTemplate.getForEntity(
+			new URL("http://localhost:" + port + "/log").toString(), String.class
+		);
+		
+		logger.info("Expected: {}", expected);
+		logger.info("Actual: {}", response.getBody());
+		assertEquals(HttpStatus.OK, response.getStatusCode());
+		JSONAssert.assertEquals(expected, response.getBody(), false);
+		
+		verify(mockRepository, times(1)).findAll();
+	}
 	
 	@Test
 	public void getHome() throws Exception {
